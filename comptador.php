fa51390c789aa23e02cb3bebfcae4429138b65c7
DFonseca-ASIX <121024852+DFonseca-ASIX@users.noreply.github.com>
2022-12-19 12:25:34 +0100

    Fonseca

--- comptador.php
+++ comptador.php
@@ -1,1 +1,10 @@
-echo "Sexe"
+<?php
+    $count=$COOKIE['count'];
+    if(isset($count)){
+        $count++;
+    }else {
+        $count=0;
+    }
+
+    setcookie('count',$count);
+    echo $count;
